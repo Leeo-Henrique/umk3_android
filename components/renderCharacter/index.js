dd935e36123b2c539968fa24839e156e6238034c
Leo <yleeoembd@gmail.com>
2024-05-27 00:34:58 -0300

    add movelist to characters

--- components/renderCharacter/index.js
+++ components/renderCharacter/index.js
@@ -1,23 +1,37 @@
 import React, { useEffect } from "react";
 import { View, Image, FlatList, Text, Dimensions } from "react-native";
-import { Card, Container,TextChar,SpecialTexts, MovesChar } from "./styles";
+import { Card, Container, TextChar, SpecialTexts, MovesChar } from "./styles";
 import { removeNameFromString } from "../utils/removeNameFromString";
 import { replaceToRightButtons } from "../utils/changeButtons";
 import { imagens } from "../utils/importImages";
 
 const RenderCharacter = ({ data, controlStyle = "snes", theme }) => {
-  const name = removeNameFromString(data.image)
+  const name = removeNameFromString(data.image);
   useEffect(() => {
     replaceToRightButtons(data, controlStyle);
   }, [data]);
 
   const { width, height } = Dimensions.get("window");
 
-  
   return (
     <Container width={width}>
       <Image style={Card.image} source={imagens[name]}/>
       <TextChar theme={theme}>{data.name}</TextChar>
+
+      <SpecialTexts theme={theme}>Especiais</SpecialTexts>
+      <FlatList
+        data={data.moveList}
+        renderItem={({ item }) => (
+          <View>
+            <MovesChar theme={theme}>
+              {" "}
+              {item.name} {item.combination}
+            </MovesChar>
+          </View>
+        )}
+        keyExtractor={(item, i) => i.toString()}
+      />
+
       <SpecialTexts theme={theme}>Combos</SpecialTexts>
       <FlatList
         data={data.combos}
